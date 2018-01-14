c266eaddf333095a9d7f1b6017133775b9d62eb8
Jacob McSwain <jacob.a.mcswain@gmail.com>
2018-01-14 09:43:07 -0600

    Let target override gpt-utils

--- gpt-utils/Android.mk
+++ gpt-utils/Android.mk
@@ -16,6 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
+ifeq ($(TARGET_PROVIDES_GPT_UTILS),)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := gpt-utils.cpp
 ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
@@ -41,3 +42,4 @@ LOCAL_MODULE_OWNER := qti
 LOCAL_COPY_HEADERS_TO := gpt-utils/inc
 LOCAL_COPY_HEADERS := gpt-utils.h
 include $(BUILD_SHARED_LIBRARY)
+endif
